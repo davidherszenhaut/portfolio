6bc848ccb71fddf9b73e10fc872efc6d8a7013e5
David Herszenhaut <david.herszenhaut@gatech.edu>
2020-01-26 17:39:03 -0500

    Planning layout in README.md and playing around with JSX

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 // @format
 import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
-export default () => <div>Hello world!
-</div>
+import data from "../../content/projects.json"
+
+console.log(data)
+
+//export default () => (
+//  <div>
+//    Hello world!
+//    <h1>{data.text.title}</h1>
+//    <h2>{data.text.subtitle}</h2>
+//    <p>{data.text.caption}</p>
+//  </div>
+//)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>{data.text.title}</h1>
+        <h2>{data.text.subtitle}</h2>
+        <p>{data.text.caption}</p>
+        <p>test</p>
+        <Footer />
+      </div>
+    )
+  }
+}
